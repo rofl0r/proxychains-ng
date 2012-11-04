859dabb302688a60f048fb1ec8e61b0cd8d5c573
rofl0r <retnyg@gmx.net>
2012-11-04 04:57:45 +0100

    common.h: supply a default for SYSCONFDIR if not passed via CFLAGS

--- src/common.h
+++ src/common.h
@@ -2,7 +2,12 @@
 #define PROXYCHAINS_QUIET_MODE_ENV_VAR "PROXYCHAINS_QUIET_MODE"
 #define PROXYCHAINS_CONF_FILE "proxychains.conf"
 #define LOG_PREFIX "[proxychains] "
+#ifndef SYSCONFDIR
+#define SYSCONFDIR "/etc"
+#endif
 
 #include <stddef.h>
 
 char *get_config_path(char* default_path, char* pbuf, size_t bufsize);
+
+//RcB: DEP "common.c"
