8ce1e51a60da1666b3911d68531195faed213605
rofl0r <retnyg@gmx.net>
2012-11-04 05:10:50 +0100

    add some test/debug programs

---
+++ tests/test_gethostent.c
@@ -0,0 +1,24 @@
+#include <netdb.h>
+#include <stdio.h>
+#include "../src/core.h"
+
+void printhostent(struct hostent *hp) {
+	char ipbuf[16];
+	pc_stringfromipv4(hp->h_addr_list[0], ipbuf);
+	printf("alias: %p, len: %d, name: %s, addrlist: %p, addrtype: %d, ip: %s\n", 
+		hp->h_aliases, 
+		hp->h_length,
+		hp->h_name,
+		hp->h_addr_list,
+		hp->h_addrtype,
+		ipbuf
+	);
+}
+
+int main(int argc, char** argv) {
+	struct hostent *hp;
+	while((hp = gethostent())) {
+		printhostent(hp);
+	}
+	return 0;
+}
