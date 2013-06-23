5c4c166802b5fdaf5072518feb19a3c0a5c216c8
crass <crass@berlios.de>
2013-06-23 12:12:25 +0700

    Add DUMP_PROXY_CHAIN for debug builds and debug.c.

--- src/common.h
+++ src/common.h
@@ -11,6 +11,10 @@
 
 #include <stddef.h>
 
+extern const char *proxy_type_strmap[];
+extern const char *chain_type_strmap[];
+extern const char *proxy_state_strmap[];
+
 char *get_config_path(char* default_path, char* pbuf, size_t bufsize);
 void pc_stringfromipv4(unsigned char *ip_buf_4_bytes, char *outbuf_16_bytes);
 
