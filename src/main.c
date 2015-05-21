204100befb3a7e013ea4185eb77166c1a66af714
Pranav Kant <pranav913@gmail.com>
2015-05-21 18:46:00 +0530

    fix missing braces

--- src/main.c
+++ src/main.c
@@ -49,13 +49,13 @@ static void set_own_dir(const char *argv0) {
 	size_t l = strlen(argv0);
 	while(l && argv0[l - 1] != '/')
 		l--;
-	if(l == 0)
+	if(l == 0) {
 #ifdef SUPER_SECURE
 		memcpy(own_dir, "/dev/null/", 2);
 #else
 		memcpy(own_dir, ".", 2);
 #endif
-	else {
+   } else {
 		memcpy(own_dir, argv0, l - 1);
 		own_dir[l] = 0;
 	}
