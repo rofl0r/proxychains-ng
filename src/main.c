6dcaf533d111c70561ff2c9b1745af2cfcf92638
Carlos Maddela <e7appew@gmail.com>
2016-12-05 18:37:11 +1100

    Fix grammar in usage message to keep Lintian happy.

--- src/main.c
+++ src/main.c
@@ -24,7 +24,7 @@
 static int usage(char **argv) {
 	printf("\nUsage:\t%s -q -f config_file program_name [arguments]\n"
 	       "\t-q makes proxychains quiet - this overrides the config setting\n"
-	       "\t-f allows to manually specify a configfile to use\n"
+	       "\t-f allows one to manually specify a configfile to use\n"
 	       "\tfor example : proxychains telnet somehost.com\n" "More help in README file\n\n", argv[0]);
 	return EXIT_FAILURE;
 }
