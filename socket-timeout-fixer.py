c34ece9066eac33b2c8cf8998e50c601caa93620
Razyar Saeedian <razyarsaeedian@gmail.com>
2020-06-20 07:35:12 -0400

    socket timeout fixer
    this error happen when tor not working fine
    this script will remove tor service and reinstall it to fix this issue

---
+++ socket-timeout-fixer.py
@@ -0,0 +1,21 @@
+'''
+this will fix fuckin socket timout 
+run this under su
+'''
+
+
+import os
+import sys
+from os import system
+
+
+def install(rep):
+  system('sudo apt-get install %s' % str(rep))
+  
+  
+def uninstall(rep):
+  system('sudo apt-get autoremove %s' % str(rep))
+  
+ 
+uninstall('tor')
+install('tor')
